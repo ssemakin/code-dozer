3294fc7e7b2092043bce7bdc387c754e59e30f95
ssemakin <ssemakin@gmail.com>
2014-08-19 23:08:52 +0300

    added clojure lazy qsort

---
+++ clojure/lazy-qsort/src/main.clj
@@ -0,0 +1,18 @@
+(ns main
+  (:gen-class))
+
+(defn sort-parts [work]
+  (lazy-seq
+    (loop [[part & parts] work]
+      (if-let [[pivot & xs] (seq part)]
+        (let [smaller? #(< % pivot)]
+          (recur (list*
+                   (filter smaller? xs)
+                   pivot
+                   (remove smaller? xs)
+                   parts)))
+        (when-let [[x & parts] parts]
+          (cons x (sort-parts parts)))))))
+
+(defn lazy-qsort [xs]
+  (sort-parts (list xs)))
